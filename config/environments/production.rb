1636849b4354387548a88c8e9309dd6b6886c23a
Alexander Ekdahl <ekdahlsandor@gmail.com>
2013-06-19 01:18:53 +0200

    Add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Bookmarket::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => "bookmarket.herokuapp.com" }
 end
