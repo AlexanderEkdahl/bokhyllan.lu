218466ebea20b0c735bbcdc4d45051a64e707068
Alexander Ekdahl <alexander@ekdahl.io>
2013-08-01 17:32:46 +0200

    Added Gmail as SMTP host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,15 @@ Bokhyllan::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["g"]
+  }
+
   config.action_mailer.default_url_options = { :host => "bokhyllan.herokuapp.com" }
 end
