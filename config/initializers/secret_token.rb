a2d499bcf0f152c612b3bbebb0b366336bda6ae2
Alexander Ekdahl <alexander@ekdahl.io>
2013-08-03 00:25:05 +0200

    Fix failing tests

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bokhyllan::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'secret'
+Bokhyllan::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || '5d94a2a62aa19e7d9b55u8w224c2bdau27dla2bewj'
