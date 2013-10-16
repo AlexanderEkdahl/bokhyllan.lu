87210a4574fbbdc52f0f7f7e495dd589ff4d59c6
Alexander Ekdahl <alexander@ekdahl.io>
2013-10-16 17:54:00 +0200

    Fixed translation and item indexing

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ActiveRecord::Base
 
   def search_data
     {
-      name: name.split,
+      name: name,
       authors: authors.try(:map, &:split),
       courses: courses.try(:map, &:split)
     }
