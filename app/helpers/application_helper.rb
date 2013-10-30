6192026d99919682be012fad86b4e8caa0eacf3b
Alexander Ekdahl <alexander@ekdahl.io>
2013-10-30 22:29:28 +0100

    ruby 1.9 style hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
   def standard_form_for(name, *args, &block)
     options = args.extract_options!
 
-    form_for(name, *(args << options.merge(:builder => StandardForm, html: { class: 'form' })), &block)
+    form_for(name, *(args << options.merge(builder: StandardForm, html: { class: 'form' })), &block)
   end
 end
