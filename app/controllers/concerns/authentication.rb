cbf56983bf627f9a3937b873bbc850614423fb06
Alexander Ekdahl <alexander@ekdahl.io>
2013-11-24 23:02:02 +0100

    Fixed fatal bug for new users

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,8 +2,6 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    before_action :authorize
-
     helper_method :current_user, :signed_in?
   end
 
@@ -15,17 +13,8 @@ module Authentication
     !cas.nil?
   end
 
-  def authorize
-    if signed_in?
-      @current_user = User.find_or_create_by(login: cas['user']) do
-        @should_alias = true
-      end
-      alias_user(@current_user) if @should_alias
-    end
-  end
-
   def current_user
-    @current_user
+    @current_user ||= User.find_or_create_by(login: cas['user']) if signed_in?
   end
 
   def authenticate
