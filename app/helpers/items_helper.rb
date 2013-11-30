b6b0d494341d6c08842d5a10ed87fd0a45b7441b
Alexander Ekdahl <alexander@ekdahl.io>
2013-11-30 14:30:28 +0100

    Refactored item properties table

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -11,18 +11,6 @@ module ItemsHelper
     item.courses.pluck(:programs).flatten.uniq.sort.to_sentence
   end
 
-  def item_i18n_t(key)
-    t("activerecord.attributes.item.#{key}")
-  end
-
-  def item_properties(item)
-    yield(item_i18n_t(:author_list), item.authors.to_sentence) unless item.authors.empty?
-    yield(item_i18n_t(:course_list), item_courses(item)) unless item.courses.empty?
-    yield(item_i18n_t(:program_list), item_programs(item)) unless item_programs(item).blank?
-    yield(item_i18n_t(:isbn), item.isbn) unless item.isbn.blank?
-    yield(item_i18n_t(:tag_list), item_tags(item)) unless item.tags.empty?
-  end
-
   def item_order_row(order)
     link = [order.item, order]
     row(link_to("#{order.price}kr", link),
