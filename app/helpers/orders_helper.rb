da311ecbef5ede5bcbc06883a1a49114a0dec141
Alexander Ekdahl <alexander@ekdahl.io>
2014-01-20 21:07:52 +0100

    order removal confirmation dialog

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -6,4 +6,8 @@ module OrdersHelper
   def other_items(order)
     order.user.orders - [order]
   end
+
+  def order_remove_button(order)
+    button_to('', [order.item, order], method: :delete, class: 'remove', onclick: "return confirm('#{t(:confirm_order_removal)}');")
+  end
 end
