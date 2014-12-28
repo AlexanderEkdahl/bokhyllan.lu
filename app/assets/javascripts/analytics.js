b9f3b1f55707ccdb806d2267d3a663f2c3ae1bba
Alexander Ekdahl <alexander@ekdahl.io>
2014-12-28 16:15:58 +0100

    stathat now ssl

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -3,6 +3,6 @@ stathat = function(ukey, key) {
   img.width = "1"
   img.height = "1"
   img.style = "display: none;"
-  img.src = "http://api.stathat.com/c?ukey=" + ukey + "&key=" + key + "&count=1"
+  img.src = "https://api.stathat.com/c?ukey=" + ukey + "&key=" + key + "&count=1"
   document.body.appendChild(img)
 }
